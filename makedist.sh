45efea104029b200bc2a38f3711fe3280d2bdbd3
Joel Stein <js@funkturm.net>
2019-08-31 02:47:20 +0200

    update makedist

--- makedist.sh
+++ makedist.sh
@@ -3,13 +3,13 @@
 rm -rf dist
 
 mkdir -p dist/unlock/amonet/bin
-cp bin/{boot0short.img,boot0.gpt,preloader.bin,minisu.img,busybox,lk.bin,tz.img,twrp.img,boot.hdr,boot.payload} dist/unlock/amonet/bin/
+cp bin/{boot0-short.bin,boot0.gpt,preloader.bin,minisu.img,busybox,lk.bin,tz.img,twrp.img,microloader.bin} dist/unlock/amonet/bin/
 
 mkdir -p dist/unlock/amonet/lk-payload/build
 cp lk-payload/build/payload.bin dist/unlock/amonet/lk-payload/build/
 
 mkdir -p dist/unlock/amonet/modules
-cp modules/{common.py,gpt.py,handshake.py,handshake2.py,load_payload.py,logger.py,main.py} dist/unlock/amonet/modules/
+cp modules/{common.py,handshake.py,handshake2.py,load_payload.py,logger.py,main.py} dist/unlock/amonet/modules/
 
 mkdir -p dist/unlock/amonet/brom-payload/build
 cp brom-payload/build/payload.bin dist/unlock/amonet/brom-payload/build/
